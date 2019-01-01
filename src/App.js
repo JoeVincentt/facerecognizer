c9352d5762c6f176cb0fcca77109f7f004952d3d
JoeVincentt <eugene.bb@hotmail.com>
2019-01-01 10:58:55 -0500

    added particles.js plus some styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Particles from "react-particles-js";
 
 import "./App.css";
+
+// Overheating hardware...
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enabled: true,
+        value_area: 800
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  },
+  retina_detect: false
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <Particles className="particles" params={particlesOptions} /> */}
         <Navigation>Nav</Navigation>
         <Logo>Logo</Logo>
+        <Rank />
         <ImageLinkForm>Image Link</ImageLinkForm>
         {/* <FaceRecognition>Face Recognition</FaceRecognition> */}
       </div>
